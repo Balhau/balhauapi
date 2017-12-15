80da0970694321c31c40b4be78306156699f9e6a
balhau <balhau@balhau.net>
2017-12-15 21:08:53 +0000

    More on youtube downloader

--- balhauapi/src/bin/ex1.rs
+++ balhauapi/src/bin/ex1.rs
@@ -5,8 +5,10 @@ use balhauapi::downloader::Downloadable;
 
 fn main(){
     let movie = YoutubeDownloader{
-        url : String::from("my lovely movie")
+        url : String::from("my lovely movie"),
+        path: String::from("/tmp")
     };
+
 
     let res = movie.download();
     println!("Res: {}",res);
--- balhauapi/src/downloader/youtube.rs
+++ balhauapi/src/downloader/youtube.rs
@@ -2,22 +2,35 @@ use super::Downloadable;
 use std::process::Command;
 
 pub struct YoutubeDownloader {
-    pub url : String
+    pub url : String,
+    pub path: String
 }
 
 /**
-* Implement the trait for
+* Implement the trait Downloadable for the YoutubeDownloader struct
 */
 impl Downloadable<bool> for YoutubeDownloader {
     fn download(&self) -> bool {
-        println!("Launching command");
+        println!("Fetching video: {} into {}",self.url,self.path);
+
+        let mut command = "".to_string();
+        let splited : Vec<&str> = self.url.split("&").collect();
+        let parsed_url = splited.get(0).unwrap();
+
+        command.push_str("cd ");
+        command.push_str(&self.path);
+        command.push_str("; youtube-dl ");
+        command.push_str("\"");
+        command.push_str(parsed_url);
+        command.push_str("\"");
 
         let output = Command::new("sh")
             .arg("-c")
-            .arg("ls -lh")
+            .arg(&command)
             .output().expect(&self.url);
-
-        println!("{}",String::from_utf8(output.stdout).unwrap());
+        //Print command executed
+        println!("Command executed: {}", command);
+        println!("Result: {:?}",output.stdout);
         true
     }
 }
