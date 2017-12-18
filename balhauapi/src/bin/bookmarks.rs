8b4a0765c4a0a689714a345e3aa1431632cf6a34
balhau <balhau@balhau.net>
2017-12-18 23:24:30 +0000

    More on bookmarks api documentation

--- balhauapi/src/bin/bookmarks.rs
+++ balhauapi/src/bin/bookmarks.rs
@@ -4,6 +4,15 @@ use clap::{App, Arg, SubCommand};
 
 fn main() {
     let matches = App::new("Bookmarks-cli")
+        .about("\nbookmarks-cli is a command line interface for bookmarks management.
+
+Examples:
+
+    Load a list of bookmarks from google chrome
+        bookmarks load -t chrome bookmarks.html
+
+"
+)
         .arg(Arg::with_name("Config")
             .help("Set the configuration file for bookmarks-cli")
             .takes_value(true)
@@ -11,7 +20,7 @@ fn main() {
             .long("config")
         )
         .subcommand(
-            SubCommand::with_name("Load")
+            SubCommand::with_name("load")
                 .about("Load bookmarks into database")
                 .version("1.0")
                 .author("Balhau (balhau@balhau.net")
@@ -31,5 +40,16 @@ fn main() {
                 )
         ).get_matches();
 
-    println!("{}",matches.usage.unwrap());
+    //Validate load command
+    if matches.is_present("load") {
+        if !matches.is_present("input") {
+            println! ("You should provide an input file, type <command> help for more information");
+        }
+        if !matches.is_present("type"){
+            println!("You should provide the type of bookmark to process, type <command> help for more information");
+        }
+    }else{  // Change this as soon as more commands are implemented
+        println!("At the moment only load command is available, type <command> help for more information ");
+    }
+
 }
