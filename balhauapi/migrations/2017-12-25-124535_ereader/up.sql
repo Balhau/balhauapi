3dddd819e972624b3d08f2d7526d165691c2488a
balhau <balhau@balhau.net>
2017-12-25 13:52:53 +0000

    Added ereader migration files

---
+++ balhauapi/migrations/2017-12-25-124535_ereader/up.sql
@@ -0,0 +1,41 @@
+-- Your SQL goes here
+CREATE TABLE T_AUTHOR (
+  id_author INTEGER PRIMARY KEY AUTOINCREMENT,
+  f_id_author INTEGER,
+  f_name TEXT,
+  f_country TEXT
+);
+
+CREATE TABLE T_ITEM (
+  id_item INTEGER PRIMARY KEY AUTOINCREMENT,
+  f_id_item INTEGER,
+  f_etag TEXT,
+  f_item_fileformat INTEGER,
+  f_item_filetype INTEGER,
+  f_item_category_flag INTEGER,
+  f_service_id INTEGER,
+  f_lang INTEGER DEFAULT -1,
+  f_pages_number INTEGER,
+  f_current_page INTEGER,--page courante
+  f_last_read INTEGER,--derniere lecture
+  f_publication_date TEXT,
+  f_publisher TEXT,
+  f_title TEXT,
+  f_cover_uri TEXT,
+  f_description TEXT,
+  f_id_dir INTEGER,
+  f_internal_uri TEXT,
+  f_external_uri TEXT,
+  f_documenttime INTEGER,--19 janvier 2038,
+  f_filesize INTEGER,
+  f_drminfo TEXT,
+  f_expiration_date INTEGER,
+  f_ade_page_index TEXT,
+  f_author_id INTEGER,
+  f_toupdate INTEGER, -- 1 le fichier est OK 0 to update
+  f_isvalid INTEGER default 1, f_cf1 TEXT,
+  f_cf2 TEXT, f_coverisvalid INTEGER default 1,
+  f_islastpage INTEGER default 0,
+  f_preorder_date INTEGER, -- le fichier est corrompu mettre � z�ro.
+  FOREIGN KEY(f_id_dir) REFERENCES T_DIRECTORY(f_id_dir) ON DELETE CASCADE
+);
--- balhauapi/src/db/mod.rs
+++ balhauapi/src/db/mod.rs
@@ -2,3 +2,4 @@ pub mod api;
 pub mod blog;
 pub mod bookmarks;
 pub mod schema;
+pub mod ereader;
